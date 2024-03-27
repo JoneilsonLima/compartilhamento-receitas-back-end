4497104f76b207b1e08bda27a9b1938b6343e27e
joneilson pereira <joneilson84@gmail.com>
2024-03-27 12:24:04 -0300

    Initial commit

---
+++ src/main/java/com/receita/CompartilhamentoReceitasApplication.java
@@ -0,0 +1,13 @@
+package com.receita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompartilhamentoReceitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompartilhamentoReceitasApplication.class, args);
+	}
+
+}
