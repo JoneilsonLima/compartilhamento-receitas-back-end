190da96fccab270eee5a06afd00cab41b9cefc3f
joneilson pereira <joneilson84@gmail.com>
2024-03-28 19:08:06 -0300

    faet: criando api para cadastro de usuário e adicionando validação para verificação de email existente

--- src/main/java/com/receita/controller/UserController.java
+++ src/main/java/com/receita/controller/UserController.java
@@ -1,15 +1,31 @@
 package com.receita.controller;
 
 import com.receita.model.User;
+import com.receita.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping("/users")
-    public User createuser(@RequestBody User user) {
-        return user;
+    public User createuser(@RequestBody User user) throws Exception{
+        User isExist  = userRepository.findByEmail(user.getEmail());
+
+        if (isExist != null) {
+            throw new Exception("user is exist with " + user.getEmail());
+        }
+
+        User savedUser = userRepository.save(user);
+
+        return savedUser;
     }
+
+
 }
