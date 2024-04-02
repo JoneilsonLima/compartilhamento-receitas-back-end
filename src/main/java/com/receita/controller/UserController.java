25ca67051506ff124dc400b8bf41ec4639ae53c2
joneilson pereira <joneilson84@gmail.com>
2024-04-02 10:38:14 -0300

    feat: criando api para deletar usuários e buscar todos os usuários

--- src/main/java/com/receita/controller/UserController.java
+++ src/main/java/com/receita/controller/UserController.java
@@ -3,18 +3,19 @@ package com.receita.controller;
 import com.receita.model.User;
 import com.receita.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/users")
+    @PostMapping
     public User createuser(@RequestBody User user) throws Exception{
         User isExist  = userRepository.findByEmail(user.getEmail());
 
@@ -27,5 +28,17 @@ public class UserController {
         return savedUser;
     }
 
+    @DeleteMapping("/{userId}")
+    public String deleteUser(@PathVariable(value = "userId") Long userId) throws Exception {
+        userRepository.deleteById(userId);
 
+        return "User deleted sucessfully";
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        List<User> users = userRepository.findAll();
+
+        return users;
+    }
 }
