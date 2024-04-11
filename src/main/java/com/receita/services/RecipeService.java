55b6fb372e7be400cf68553c8eb7957872b6e8c7
joneilson pereira <joneilson84@gmail.com>
2024-04-10 21:14:27 -0300

    feat: criando interface para implementar no serviço de receita

--- src/main/java/com/receita/services/RecipeService.java
+++ src/main/java/com/receita/services/RecipeService.java
@@ -1,4 +1,17 @@
 package com.receita.services;
 
+import com.receita.model.Recipe;
+import com.receita.model.User;
+
+import java.util.List;
+
 public interface RecipeService {
+
+    public Recipe createRecipe(Recipe recipe, User user);
+    public Recipe findRecipeById(Long id) throws Exception;
+    public void deleteRecipe(Long id) throws Exception;
+    public Recipe updateRecipe(Recipe recipe, Long id) throws Exception;
+    public List<Recipe> findAllRecipe();
+    public Recipe likeRecipe(Long recipeId, User user) throws Exception;
+
 }
